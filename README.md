a4927b071304d13077e73b2e9c6a552a8b1f5a20
Userr1 <user1@example.com>
2025-02-19 15:05:10 +0100

    Added app changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # release-app
+
+C:\edu\java\test\release-app\target
+
+
+java -cp release-app-1.0-SNAPSHOT-jar-with-dependencies.jar com.oti.VersioningDemo
---
+++ src/main/java/com/oti/VersioningDemo.java
@@ -0,0 +1,11 @@
+package com.oti;
+
+public class VersioningDemo {
+    public static void main(String args[]) {
+        System.out.println("Hello explorers.");
+        System.out.println();
+        System.out.println("This program is running version "+
+                VersioningDemo.class.getPackage().getImplementationVersion());
+    }
+
+}
