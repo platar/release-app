2dd3db0e207e25f3870c6e9a4c7d6c73d78644d3
Userr1 <user1@example.com>
2025-02-19 15:17:01 +0100

    Chamged VersioningDemo

--- src/main/java/com/oti/VersioningDemo.java
+++ src/main/java/com/oti/VersioningDemo.java
@@ -2,7 +2,7 @@ package com.oti;
 
 public class VersioningDemo {
     public static void main(String args[]) {
-        System.out.println("Hello explorers.");
+        System.out.println("Hello explorers!");
         System.out.println();
         System.out.println("This program is running version "+
                 VersioningDemo.class.getPackage().getImplementationVersion());
