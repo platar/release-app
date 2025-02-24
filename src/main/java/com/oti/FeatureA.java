89b323f4484b1cbb2c49c59393e205d5175a8880
Userr1 <user1@example.com>
2025-02-24 15:45:56 +0100

    feature_a - Added FeatureA
    feature_a - Added getMessageTwo method
    
    feature_a - Added getMessageThree method

---
+++ src/main/java/com/oti/FeatureA.java
@@ -0,0 +1,17 @@
+package com.oti;
+
+public class FeatureA {
+    public String getMessage() {
+        return "FeatureA";
+    }
+
+
+    public String getMessageTwo() {
+        return "FeatureA-MessageTwo";
+    }
+
+
+    public String getMessageThree() {
+        return "FeatureA-MessageTwo";
+    }
+}
